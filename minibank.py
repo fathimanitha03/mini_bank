f1c192beab933761e13338475cc9657d49057ff1
Mohammed Harees Fathima Nith <UT010216@unicomtic.com>
2025-05-05 14:41:52 +0530

    my first

---
+++ minibank.py
@@ -0,0 +1,46 @@
+# To save account details
+def creating_account_number():
+    while true:
+        acct_no=str(random.randint(1000,999))
+        if acct_no not in accounts:
+            return acct_no
+
+def create_account():
+    name=int(input('Enter the Cutomer name: ')).strip()
+    try:
+        starting_balance = float(input('Enter your strting balance: '))
+        if starting_balance<0:
+            print("Starting balance must be greater than 0.")
+            return
+        except ValueError:
+            print("Please input valid number")
+            return
+
+    acct_no = creating_account_number()
+    accounts[acct_no]= {
+        "Name":name,
+        "balance":starting_balance,
+        "transactions":[f"Your account is created!{starting_balance}"]
+    }
+    print(f"Account created successfully! Your account number is {acct_no}")
+
+#deposite the money
+def deposit_money():
+    acct_no=int(input("Enter Your Account Number here: "))
+    if acct_no in accounts:
+        try:
+            amount=float(input("Enter the deposit amount: "))
+            if amount<=0:
+                print("Amount must be greater than 0")
+                return
+            accounts[acct_no]["balance"]+=amount
+            accounts[acct_no]["transaction"].append(f'Your money is deposited{amount}')
+            print("Deposited Successfully!")
+        except ValueError:
+            print("invalid amount")
+        else:
+        print("account not found")
+
+#
+
+
---
+++ te.py
@@ -0,0 +1,14 @@
+#login 
+customername = "customer1" 
+password = "cus123@" 
+input_customer = input("Enter customer name: ") 
+input_pass = input("Enter the password: ") 
+if input_customer == customername and input_pass == password: 
+ print("Welcome Login successful!\n") 
+else: 
+ print("Login failed. Exitng program.")
+ exit() 
+ def withdraw_money():
+    username=input('enter your user name')
+    userage=input('enter your age')
+    useraddress= input('enter ')
